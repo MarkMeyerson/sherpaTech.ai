a4a7207896c64ac6da039454ddb445aca5fe4056
MarkMeyerson <mark@awarehousedc.com>
2025-05-21 19:48:34 -0400

    updatestoroot

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true
+  },
   build: {
-    rollupOptions: {
-      external: ['@rollup/rollup-linux-x64-gnu']
-    }
+    outDir: 'dist',
+    sourcemap: true
   }
 });
