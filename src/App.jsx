5d3ea3f89b2836a882f5fc884cc948cffd68c1c4
MarkMeyerson <mark@awarehousedc.com>
2025-05-21 21:34:44 -0400

    erroreronerrore

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { theme } from "./theme";
-import './App.css';
+// Remove App.css import since we're using index.css
 import './index.css';
 
 // Components
