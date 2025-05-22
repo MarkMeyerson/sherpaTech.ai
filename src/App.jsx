d27c4a38a0b213d4e2d73446f41a4d18648d240d
MarkMeyerson <mark@awarehousedc.com>
2025-05-21 21:18:37 -0400

    momomo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { theme } from './theme';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { theme } from "./theme";
 import './App.css';
 import './index.css';
 
