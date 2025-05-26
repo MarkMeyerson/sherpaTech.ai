fa019b960714b8886663c3d99c835674c5da29f7
MarkMeyerson <mark@awarehousedc.com>
2025-05-26 14:03:50 -0400

    debub logging

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AssessmentTest } from './pages/AssessmentTest';
+import { SmallBusinessHero } from './components/SmallBusinessHero';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/assessment-test" element={<AssessmentTest />} />
+        <Route path="/" element={<SmallBusinessHero />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
+// Inside SmallBusinessHero.tsx
+const handleStartAssessment = () => {
+  console.log('Start Assessment button clicked');
+};
+
+// Inside Assessment.tsx
+console.log('Assessment loaded, current question:', INITIAL_QUESTIONS[0]);
