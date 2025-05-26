25aabaaf7aaa192fa99f4f73e09cddc04eeb07a5
MarkMeyerson <mark@awarehousedc.com>
2025-05-26 12:21:56 -0400

    testing th test

--- src/pages/AssessmentTest.tsx
+++ src/pages/AssessmentTest.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Assessment } from '../features/assessment/components/Assessment';
 
 export const AssessmentTest: React.FC = () => {
   return (
     <div className="container mx-auto py-8">
       <h1 className="text-2xl font-bold mb-6">Assessment Test Page</h1>
-      <Assessment />
+      <p>If you see this, the page is rendering correctly.</p>
     </div>
   );
 };
