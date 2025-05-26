d47b7cd89f0548958b82f060cb082f144a9a7b9d
MarkMeyerson <mark@awarehousedc.com>
2025-05-26 12:17:06 -0400

    mo changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AssessmentTest } from './pages/AssessmentTest';
 
 function App() {
   return (
-    <Routes>
-      {/* ...existing routes... */}
-      <Route path="/assessment-test" element={<AssessmentTest />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/assessment-test" element={<AssessmentTest />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/features/assessment/components/Assessment.tsx
+++ src/features/assessment/components/Assessment.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { INITIAL_QUESTIONS } from '../constants/questions';
 
 export const Assessment: React.FC = () => {
+  console.log('INITIAL_QUESTIONS:', INITIAL_QUESTIONS);
+
   return (
     <div>
       <h2>Assessment Component Loaded</h2>
+      <p>Check the console for INITIAL_QUESTIONS.</p>
     </div>
   );
 };
